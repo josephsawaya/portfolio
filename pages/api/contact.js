e6b8f9b82bdf55e0be8f9c916793352a527e6419
Joseph Sawaya <josephsawaya938@gmail.com>
2020-07-27 21:11:57 -0400

    fixing api bug

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,24 +13,24 @@ export default function (req, res) {
     res.statusCode = 400;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ err: true }));
+  } else {
+    const mailOptions = {
+      from: "Joseph Sawaya <contact-form@josephsawaya.com>",
+      to: "josephsawaya938@gmail.com",
+      subject: "Invoices due",
+      text: JSON.stringify(req.body),
+    };
+
+    transport.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        res.statusCode = 400;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ err: true, message: err.message }));
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ err: false, body: req.body }));
+      }
+    });
   }
-
-  const mailOptions = {
-    from: "Joseph Sawaya <contact-form@josephsawaya.com>",
-    to: "josephsawaya938@gmail.com",
-    subject: "Invoices due",
-    text: JSON.stringify(req.body),
-  };
-
-  transport.sendMail(mailOptions, function (err, info) {
-    if (err) {
-      res.statusCode = 400;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ err: true, message: err.message }));
-    } else {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ err: false, body: req.body }));
-    }
-  });
 }
