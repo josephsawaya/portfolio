d3d9ea2ae1e6050833cf2889f4099c50325ad060
Joseph Sawaya <josephsawaya938@gmail.com>
2020-07-27 23:32:07 -0400

    added rate limiting middleware

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,10 @@
 import nodemailer from "nodemailer";
+import * as rateLimiter from "express-rate-limit";
+
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 15, // limit each IP to 100 requests per windowMs
+});
 
 const transport = nodemailer.createTransport({
   service: "gmail",
@@ -8,12 +14,27 @@ const transport = nodemailer.createTransport({
   },
 });
 
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 export default function (req, res) {
   if (req.method !== "POST") {
     res.statusCode = 400;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ err: true }));
   } else {
+    console.log("made it ");
+    runMiddleware(req, res, limiter);
+    console.log("ran middleware");
+
     const mailOptions = {
       from: "Joseph Sawaya <contact-form@josephsawaya.com>",
       to: "josephsawaya938@gmail.com",
