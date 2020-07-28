2b7f526ddef1a2df5cdb8faf7a5708960ab4bf13
Joseph Sawaya <josephsawaya938@gmail.com>
2020-07-28 02:47:14 -0400

    styling contact form

---
+++ components/form.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import styles from "./form.module.css";
+import Head from "next/head";
+
+const Form = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const checkFormValidity = (e) => {
+    e.preventDefault();
+    if (!name || !(email || phoneNumber) || !message) {
+      console.log("invalid");
+    } else {
+      console.log("valid");
+      const data = {
+        name,
+        email,
+        message,
+      };
+      fetch("api/contact", {
+        method: "post",
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => {
+        console.log(JSON.stringify(res));
+      });
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+  return (
+    <form className={styles.form} onSubmit={checkFormValidity}>
+      <div className={styles.field}>
+        <label className={styles.label}>Name</label>
+        <input
+          className={styles.input}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+      </div>
+      <div className={styles.field}>
+        <label className={styles.label}>Email</label>
+        <input
+          className={styles.input}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+
+      <div className={styles.messageField}>
+        <label className={styles.label}>Message</label>
+        <textarea
+          className={styles.messageInput}
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        ></textarea>
+      </div>
+      <input className={styles.submit} type="submit"></input>
+    </form>
+  );
+};
+
+export default Form;
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
-import { useState } from "react";
+import Form from "../components/form";
 export default function Home() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhone] = useState("");
-  const [message, setMessage] = useState("");
-
-  function checkFormValidity(e) {
-    e.preventDefault();
-    if (!name || !(email || phoneNumber) || !message) {
-      console.log("invalid");
-    } else {
-      console.log("valid");
-      const data = {
-        name,
-        email,
-        phoneNumber,
-        message,
-      };
-      fetch("api/contact", {
-        method: "post",
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }).then((res) => {
-        console.log(JSON.stringify(res));
-      });
-      setName("");
-      setEmail("");
-      setPhone("");
-      setMessage("");
-    }
-  }
-
   return (
     <div className="container">
       <Head>
@@ -57,20 +22,7 @@ export default function Home() {
       <div id="projects"></div>
       <div id="about-me"></div>
       <div id="contact">
-        <form onSubmit={checkFormValidity}>
-          <label>Name</label>
-          <input onChange={(e) => setName(e.target.value)} value={name} />
-          <label>Email</label>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} />
-          <label>Phone number</label>
-          <input
-            onChange={(e) => setPhone(e.target.value)}
-            value={phoneNumber}
-          />
-          <label>Message</label>
-          <input onChange={(e) => setMessage(e.target.value)} value={message} />
-          <input type="submit"></input>
-        </form>
+        <Form />
       </div>
       <style jsx>{``}</style>
     </div>
