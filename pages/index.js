0c95c871d56402b42aebaee71fae40789ea5edc6
Joseph Sawaya <josephsawaya938@gmail.com>
2020-08-21 19:23:53 -0400

    99% sure it's fixed

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,8 @@ export default function Home({ contentHtml, projects }) {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale = 1.0, 
+maximum-scale=1.0, user-scalable=no"
           />
         </Head>
         <div className="navbar">
